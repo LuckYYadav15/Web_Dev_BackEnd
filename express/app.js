bcceff570969b8f45ebadcbfa0bae0d4c3271c20
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-11-23 01:55:12 +0530

    Route Params

--- express/app.js
+++ express/app.js
@@ -1,2 +1,42 @@
-// Route Params :- used to iterate mutilple files to open as in file_19
-// if shows error then copy the "express" folder on desktop to run the file
+// query request
+
+const express = require('express');
+const app = express();
+
+
+//web address: http://localhost:5000/api/v1/query?name=cena&id=15
+//http://localhost:5000/api/v1/query?search=a&limit=2
+
+// app.get('/api/v1/query', (req, res) => {
+//     console.log(req.query);
+//     res.send("hello world")
+// })
+
+
+
+// web address: http://localhost:5000/api/v1/query?limit=2
+// http://localhost:5000/api/v1/query?search=a
+// http://localhost:5000/api/v1/query?search=a&limit=1
+
+const {products} = require('./data');
+
+app.get('/api/v1/query',(req, res) =>{
+    const {search, limit} = req.query;
+    let sortedProducts = [...products]
+    if (search) {
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if (sortedProducts.length < 1) {
+        //res.status(200).send("no products matched to your search");
+        res.status(200).json({success: true, data: []})
+    }
+    res.status(200).json(sortedProducts)
+})
+app.listen(5000, () => {
+    console.log("Listening on port 5000")
+})
--- express/file_19.js
+++ express/file_19.js
@@ -1,25 +1,17 @@
-// API Vs SSR
-// API-json -> javascript object notation  {RES.JSON()}
-// SSR-template -> send tempelate  {RES.RENDER()}
+// Route Params :- used to iterate mutilple files to open as in file_19
 // if shows error then copy the "express" folder on desktop to run the file
+
 
 const express = require('express');
 const app = express();
 
 const {products} = require('./data');
 
-// app.get('/', (req, res)=>{
-//     res.json({name: 'janam'}, {name: 'jaadu'})
-// })
-
-// app.get('/', (req, res)=>{
-//     res.json(products);
-// })
 
 app.get('/', (req, res) => {
     res.send('<h1> Home Page</h1><a href = "/api/products"> products</a>');
 })
-// to setup get request for /api/products resource in this function it just shows the id, image ane name not the entire discription so it looks good
+
 
 app.get('/api/products', (req, res)=>{
     const newProducts = products.map((products) =>{
@@ -29,32 +21,23 @@ app.get('/api/products', (req, res)=>{
     res.json(newProducts)
 })
 
-// now to get discription of produc 1
-app.get('/api/products/1', (req, res)=>{
-    const singleProduct = products.find((product) => product.id ===1)
+app.get('/api/products/:productID', (req, res)=>{
+    // console.log(req);
+    // console.log(req.params);
+    const { productID } = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productID))
+    // default case
+    if (!singleProduct) {
+        return res.status(404).send("Product Does Not Exist.")
+    }
     res.json(singleProduct)
 })
 
-// discription of product 2
-app.get('/api/products/2', (req, res)=>{
-    const singleProduct = products.find((product) => product.id ===2)
-    res.json(singleProduct)
+// nesting things with review and review id
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    res.send("This is a review........")
 })
-
-
-// discription of product 3
-app.get('/api/products/3', (req, res)=>{
-    const singleProduct = products.find((product) => product.id ===3)
-    res.json(singleProduct)
-})
-
-
-// discription of product 4
-app.get('/api/products/4', (req, res)=>{
-    const singleProduct = products.find((product) => product.id ===4)
-    res.json(singleProduct)
-})
-
 
 
 
