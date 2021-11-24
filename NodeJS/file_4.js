661ee9f03c60ed83e7e7456f0e61d2bb12437ec7
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-11-24 18:02:21 +0530

    updates

--- NodeJS/file_4.js
+++ NodeJS/file_4.js
@@ -1,6 +1,18 @@
-module.exports.array = ['data1', 'data2', 'data3', 'data4'];
-module.exports.fun = {
-    name: "Name",
-    roll: "roll",
+// module.exports.array = ['data1', 'data2', 'data3', 'data4'];
+// module.exports.fun = {
+//     name: "Name",
+//     roll: "roll",
+// }
+// object
+
+// objects
+a = ["Yuvi", "Karan", "Boman"]
+const person = {
+    name: a[0],
+    age: 21,
+    greet() {
+        console.log( 'hi, i am ' + this.name )
+    }
 }
+person.greet()
 //database to be used in other files
