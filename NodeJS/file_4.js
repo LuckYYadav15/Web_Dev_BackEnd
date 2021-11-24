2183eb507702dea2b76a06452b9d8c873f55a9bd
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-11-24 19:44:26 +0530

    destructures

--- NodeJS/file_4.js
+++ NodeJS/file_4.js
@@ -15,4 +15,38 @@ const person = {
     }
 }
 person.greet()
+
+// editing array
+console.log(a.map((username, surname)=> "Name: " + username + surname))
+console.log('this is original a: ' + a)
 //database to be used in other files
+
+// nested array: spread and rest
+const nestedarr = [a]
+console.log("this is nestedarr: " + nestedarr)
+
+// array with values of "a" and we can add more values to it
+const b = a.slice();
+b.push("hariram");
+console.log("this is new array: b"+ b)
+
+const copiedarr = [...a];
+console.log("this is copiedarr: " + copiedarr);
+
+const copiedperson = {...person}
+console.log("copied object: " + copiedarr)
+
+// allows to pass as many as values to function
+const fun = (...args) =>{
+    return [args]
+}
+console.log(fun(1,2,3,4,546,"sadf"))
+
+
+// Destructuring
+const print = ({name, age}) => {
+    console.log(name, age);
+}
+print(person);
+const [name1, name2] = a;
+console.log(name1, name2);
