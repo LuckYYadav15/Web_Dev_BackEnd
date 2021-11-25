9045201d428f4e4a5de33e82cb97f406ad64c2b6
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-11-25 23:59:13 +0530

    server modefied

--- NodeJS/file_19.js
+++ NodeJS/file_19.js
@@ -5,8 +5,8 @@ const server = http.createServer((req, res) =>{
     if(url === '/'){
         res.write('<html>');
         res.write('<head><title>Enter Message </title><head>');
-        res.write('<body><form = action "/message" method = "POST"><input type ="text"><button type = "submit">Send</button> name = "message"></body>');
-        res.write('/html');
+        res.write('<body><form action = "/message" method = "POST"><input type ="text" name = "message"><button type = "submit">Send</button> name = "message"></body>');
+        res.write('</html>');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
@@ -15,5 +15,5 @@ const server = http.createServer((req, res) =>{
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
-    server.listen(5000);
+    server.listen(3000);
 })
