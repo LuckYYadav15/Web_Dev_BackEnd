c7a2c7905935bbfc8a6452b7357d1d6cb53610b0
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-11-28 00:00:50 +0530

    admin_1 updated

--- express/routes/admin_1.js
+++ express/routes/admin_1.js
@@ -14,7 +14,7 @@ router.get('/add-product',(req, res, next) => {
 });
 
 // admin/add-product => POST
-router.post('/product',(req, res, next) => {
+router.post('/add-product',(req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
