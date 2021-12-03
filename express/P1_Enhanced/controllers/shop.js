5c15777ea52fb57252722f23ee5e8e66e9bd2a37
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-03 10:31:28 +0530

    added delete cart

--- express/P1_Enhanced/controllers/shop.js
+++ express/P1_Enhanced/controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart')
+const Cart = require('../models/cart');
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -10,17 +11,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct = (req, res, next)=>{
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail',{
-      product:product,
-      pageTitle:product.title,
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
       path: '/products'
     });
   });
-  res.redirect('/');
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -41,12 +41,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) =>{
+  Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price);
   });
-  console.log(prodId);
   res.redirect('/cart');
-}
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
--- express/P1_Enhanced/routes/shop.js
+++ express/P1_Enhanced/routes/shop.js
@@ -10,7 +10,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId', shopController.getProduct);//dynamic to be kept at last
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
