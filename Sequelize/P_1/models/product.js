662e6ba2bef52ce5fff35f384d3f88a2f43fa02d
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-04 17:39:47 +0530

    Starting With Sequelize

---
+++ Sequelize/P_1/models/product.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize'); // class or constructor function
+
+const sequelize = require('../util/database');  // database connection tool
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Product;
---
+++ Sequelize/P_1/util/database.js
@@ -0,0 +1,4 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', '1234', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
