999f780949f2fc8e785bfa773c579ce8a51a8540
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-24 00:39:49 +0530

    Admin products added

--- Sequelize/P_1/controllers/admin.js
+++ Sequelize/P_1/controllers/admin.js
@@ -65,13 +65,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
