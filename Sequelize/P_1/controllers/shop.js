b72d6b4eb7e8b2dbc43d27eebadaf0a0cb32d561
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-24 00:34:47 +0530

    Details bug resolved

--- Sequelize/P_1/controllers/shop.js
+++ Sequelize/P_1/controllers/shop.js
@@ -17,15 +17,26 @@ exports.getProducts = (req, res, next) => {
 //details
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
-        path: '/products'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll({where: {id: prodId}})
+  .then(products => {
+    res.render('shop/product-detail', {
+      product: products[0],
+      pageTitle: products[0].title,
+      path: '/products'
+    });
+  })
+  .catch(err => console.log(err));
+  // Method-2 using findByPk which gives a single element
+
+  // Product.findByPk(prodId)
+  //   .then(product => {
+  //     res.render('shop/product-detail', {
+  //       product: product,
+  //       pageTitle: product.title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
 };
 
 // shop homepage
