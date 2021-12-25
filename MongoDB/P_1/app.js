59e8665c73ae92fbdca6a5e163befce7b02f9a2a
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-25 16:22:10 +0530

    finishing database connection

--- MongoDB/P_1/app.js
+++ MongoDB/P_1/app.js
@@ -31,7 +31,6 @@ app.use('/admin', adminRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client => {
-  //console.log(client);
+mongoConnect(() => {
   app.listen(3000);
 });
--- MongoDB/P_1/util/database.js
+++ MongoDB/P_1/util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;// temp var to be used in this file
+
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://User-1:1@cluster0.4opu0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://User-1:1@cluster0.4opu0.mongodb.net/shop?retryWrites=true&w=majority')
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();// connects to 'shop' mongodb.net/shop?retryWrites=true
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
