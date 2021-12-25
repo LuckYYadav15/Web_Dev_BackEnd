73023996b0da70864022f0f1bece43ac52ef7d63
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-25 16:40:54 +0530

    creating products

--- MongoDB/P_1/controllers/admin.js
+++ MongoDB/P_1/controllers/admin.js
@@ -13,13 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
+  const product = new Product(title, price, description, imageUrl);
+  product
+    .save()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
