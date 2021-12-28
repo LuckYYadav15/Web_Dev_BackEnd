3de7097c41c166df61b66c8523c1f328523b0d99
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-28 23:33:23 +0530

    delete product

--- MongoDB/P_1/models/product.js
+++ MongoDB/P_1/models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
@@ -55,6 +55,16 @@ class Product {
       .then(product => {
         console.log(product);
         return product;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
       })
       .catch(err => {
         console.log(err);
