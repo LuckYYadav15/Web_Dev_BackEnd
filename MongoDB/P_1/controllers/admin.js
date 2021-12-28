3ce8d88dd768b737c8dba24890e687dab660d24a
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-28 23:20:20 +0530

    product model changed

--- MongoDB/P_1/controllers/admin.js
+++ MongoDB/P_1/controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require('mongodb');
 const Product = require('../models/product');
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -59,7 +56,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
 
   const product = new Product(
-    updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, new ObjectId(prodId)
+    updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId
   );
   product
     .save()
