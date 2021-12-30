cd403565f3f1c12129c2f29f12703b45d9c53fa1
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-30 17:11:34 +0530

    add to cart

--- MongoDB/P_1/models/user.js
+++ MongoDB/P_1/models/user.js
@@ -20,11 +20,11 @@ class User {
     //   return cp._id === product._id;
     // });
 
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const updatedCart = { items: [{productId: new ObjectId(product._id), quantity: 1 }] };
     const db = getDb();
     return db
       .collection('users')
-      .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });// overwrite new cart with old one
+      .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });// overwrite old cart with new one
   }
 
   static findById(userId) {
