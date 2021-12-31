45c337392929f27cbbeae05d3445b32e4fccacd0
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-31 16:29:27 +0530

    display cart

--- MongoDB/P_1/controllers/shop.js
+++ MongoDB/P_1/controllers/shop.js
@@ -53,17 +53,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
--- MongoDB/P_1/models/user.js
+++ MongoDB/P_1/models/user.js
@@ -17,9 +17,9 @@ class User {
 
   addToCart(product) {
     const cartProductIndex = this.cart.items
-    .findIndex(cp => {
-      return cp.productId.toString() === product._id.toString();
-    });
+      .findIndex(cp => {
+        return cp.productId.toString() === product._id.toString();
+      });
     let newQuantity = 1;
     const updatedCartItems = [...this.cart.items];
     if (cartProductIndex >= 0) {
@@ -37,6 +37,26 @@ class User {
       .collection('users')
       .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });// overwrite old cart with new one
   }
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p, quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      });
+  }
+
 
   static findById(userId) {
     const db = getDb();
