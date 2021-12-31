aefe3fde455f24b4434de0b7dcc09bda4942b507
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2021-12-31 16:32:08 +0530

    redirect to cart bug fixed

--- MongoDB/P_1/controllers/shop.js
+++ MongoDB/P_1/controllers/shop.js
@@ -65,12 +65,14 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
+  Product.findById(prodId)
+  .then(product => {
     return req.user.addToCart(product);
   })
     .then(
       result => {
         console.log(result);
+        res.redirect('/cart');
       }
     );
   // let fetchedCart;
