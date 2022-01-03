b8d8bf245f2ba2b1cbd58d979ae0c2eb012b61f7
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-03 15:47:06 +0530

    delete cart items

--- Mongoose/models/user.js
+++ Mongoose/models/user.js
@@ -41,6 +41,14 @@ userSchema.methods.addToCart = function (product) {
     };
     this.cart = updatedCart;
     return this.save();
+};
+
+userSchema.methods.removeFromCart = function (productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
 }
 
 module.exports = mongoose.model('User', userSchema);
