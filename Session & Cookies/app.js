1f310fccb4d456ab98919ffda22486f4009d471e
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-04 22:23:22 +0530

    session connected with mongo

--- Session & Cookies/app.js
+++ Session & Cookies/app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://User-1:1@cluster0.4opu0.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +26,13 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));
 
 app.use((req, res, next) => {
   User.findById('61d1e85eeda26c08ccc75a63')
@@ -38,7 +51,8 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://User-1:1@cluster0.4opu0.mongodb.net/shop?retryWrites=true&w=majority'
+    MONGODB_URI
+
   )
   .then(result => {
     User.findOne().then(user => {
--- Session & Cookies/controllers/auth.js
+++ Session & Cookies/controllers/auth.js
@@ -1,14 +1,16 @@
 const cookieParser = require('cookie-parser');
 
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
+    // const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        // isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 };
 exports.postLogin = (req, res, next) => {
-    res.cookie("loggedIn", 'true');
+    //res.cookie("loggedIn", 'true');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };
