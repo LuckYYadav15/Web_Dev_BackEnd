c77452bac02d6e52913d92944914c8afea87bbb9
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-04 22:07:41 +0530

    initialise session

--- Session & Cookies/app.js
+++ Session & Cookies/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
 
 app.use((req, res, next) => {
   User.findById('61d1e85eeda26c08ccc75a63')
