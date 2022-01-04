2c1346a7d874cc0ad6f9284bd7a5fb77ac506464
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-04 21:34:55 +0530

    setting cookie

--- Session & Cookies/controllers/auth.js
+++ Session & Cookies/controllers/auth.js
@@ -1,14 +1,14 @@
 const cookieParser = require('cookie-parser');
 
 exports.getLogin = (req, res, next) => {
-    console.log(req.get('Cookie').split(';')[1].trim().split('=')[1]);
+    const isLoggedIn = req.get('Cookie').split('=')[1];
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     });
 };
 exports.postLogin = (req, res, next) => {
-    res.cookie("loggedIn" , 'true');
+    res.cookie("loggedIn", 'true');
     res.redirect('/');
 };
