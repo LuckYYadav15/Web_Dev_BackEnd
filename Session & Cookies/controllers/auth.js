137650c1a5d6cbe4754f5fe6af9be12dc7e235b7
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-04 21:38:58 +0530

    bug fixed

--- Session & Cookies/controllers/auth.js
+++ Session & Cookies/controllers/auth.js
@@ -1,7 +1,7 @@
 const cookieParser = require('cookie-parser');
 
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').split('=')[1];
+    const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
