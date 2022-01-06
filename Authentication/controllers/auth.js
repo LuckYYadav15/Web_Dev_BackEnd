e27228c8a564509b995fd9cacaf57a5a2cad1bb7
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-06 16:01:19 +0530

    Routes added

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -9,6 +9,14 @@ exports.getLogin = (req, res, next) => {
     });
 };
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
+
 exports.postLogin = (req, res, next) => {
     User.findById('61d1e85eeda26c08ccc75a63')
         .then(user => {
@@ -22,6 +30,7 @@ exports.postLogin = (req, res, next) => {
         .catch(err => console.log(err));
 
 };
+exports.postSignup = (req, res, next) => { };
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
