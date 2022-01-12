0f177ea0f285458f3bda2f875e3d04512621c985
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-12 23:45:31 +0530

    bug fixed

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -39,18 +39,19 @@ exports.postSignup = (req, res, next) => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
-            return bcrypt.hash(password, 12);
-        })
-        .then(hashedPassword => {
-            const user = new User({
-                email: email,
-                password: hashedPassword,
-                cart: { items: [] }
-            });
-            return user.save()
-        })
-        .then(result => {
-            res.redirect('/login');
+            return bcrypt
+                .hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save()
+                })
+                .then(result => {
+                    res.redirect('/login');
+                });
         })
         .catch(err => {
             console.log(err);
