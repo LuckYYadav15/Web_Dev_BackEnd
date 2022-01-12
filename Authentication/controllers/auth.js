78e374efa03bd58e4def76dbc92721267e9e710a
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-01-12 23:41:58 +0530

    password encrypted

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 //const cookieParser = require('cookie-parser');
-
+const bcrypt = require('bcryptjs')
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
@@ -39,12 +39,15 @@ exports.postSignup = (req, res, next) => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
+            return bcrypt.hash(password, 12);
+        })
+        .then(hashedPassword => {
             const user = new User({
                 email: email,
-                password: password,
+                password: hashedPassword,
                 cart: { items: [] }
             });
-            return user.save();
+            return user.save()
         })
         .then(result => {
             res.redirect('/login');
